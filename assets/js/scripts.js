b4abbb1447054d3f295f0339ff8d5f136e945796
Marcel Rus <marcel.rus.design@gmail.com>
2018-03-18 17:39:39 -0700

    Add event listener that runs addList() when user clicks any key on keyboard

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
 //Create variables for the mainContainer and addListInput 
 var mainContainer = document.querySelector(".main-container");
 var addListInput = document.querySelector(".add-list-input");
+
+//When the user clicks any key on the keyboard run the addList function
+addListInput.addEventListener("keypress", addList);
