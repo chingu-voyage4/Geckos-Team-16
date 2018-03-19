82efb2da41a3ea001ea433ac9bc12e6969850461
Marcel Rus <marcel.rus.design@gmail.com>
2018-03-18 17:50:33 -0700

    Rename the class names for innerHTML elements added to the newList variable

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,7 +14,7 @@ function addList(){
     var newList = document.createElement("div");
 
     //Add html code to the new div element
-    newList.innerHTML = "<div class='listContainer'><h1 class='listTitle'>" + addListInput.value + "</h1><input class='task-input' type='text' placeholder='Add a task...'></div>";
+    newList.innerHTML = "<div class='list-container'><h1 class='list-title'>" + addListInput.value + "</h1><input class='task-input' type='text' placeholder='Add a task...'></div>";
     
     //Add the new div element to the webpage
     mainContainer.appendChild(newList);
