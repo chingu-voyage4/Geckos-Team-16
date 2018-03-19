ed2e81ef6e4c936262df733642f57cc43c43ef6d
Marcel Rus <marcel.rus.design@gmail.com>
2018-03-18 17:41:08 -0700

    Create addList() function with if statement that runs when user clicks 'enter' key

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,11 @@ var addListInput = document.querySelector(".add-list-input");
 
 //When the user clicks any key on the keyboard run the addList function
 addListInput.addEventListener("keypress", addList);
+
+function addList(){
+
+  //If the user presses the enter key run the following code
+  if(event.which === 13){
+
+  }
+}
